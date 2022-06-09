7e0618f6ae1f863cf24e258550f916418baf6047
Alan192 <45466767+Alan192@users.noreply.github.com>
2022-06-09 21:08:27 +0200

    Delete ToDoListExamen directory

--- ToDoListExamen/scripts/create-db.sh
+++
@@ -1,9 +0,0 @@
-
-
-sudo mysql -h localhost -u root -p
-create database todolistexamen;
-create user todolistexamen;
-GRANT USAGE ON *.* TO 'todolistexamen'@localhost IDENTIFIED BY 'todolistexamen';
-GRANT USAGE ON *.* TO 'todolistexamen'@'%' IDENTIFIED BY 'todolistexamen';
-GRANT ALL privileges ON `todolistexamen`.* TO 'todolistexamen'@localhost;
-flush privileges;
--- ToDoListExamen/scripts/mqtt.sh
+++
@@ -1,1 +0,0 @@
-mosquitto_sub -h 127.0.0.1 -d -p 1883 -t "todolist"
